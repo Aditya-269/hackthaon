3660819c4475942a20bbbcf1579798ceb3f7f2d8
aditya kumar sahoo <adityakusahoo04@gmail.com>
2025-03-18 17:19:49 +0530

    Update RoutePlanning.tsx

--- src/components/RoutePlanning.tsx
+++ src/components/RoutePlanning.tsx
@@ -2,7 +2,7 @@ import { useEffect } from "react";
 
 export default function RedirectPage() {
   useEffect(() => {
-    window.location.href = "http://192.168.170.188:8501"; // Replace with the desired URL
+    window.location.href = "http://192.168.170.188:8501";  // Correct Streamlit URL
   }, []);
 
   return null;
